5596ba97e89bda32025dc029802bbac521de6e3d
GElsner <gerhard.elsner@googlemail.com>
2009-12-09 22:38:18 +0100

    Update: added "Scripts" to submenu and some other stuff

---
+++ extras/CopyScripts.py
@@ -0,0 +1,10 @@
+import os
+
+scriptexists = os.path.exists('/Applications/Plex.app/Contents/Resources/Plex/scripts/Alaska_OpenSubtitles_OSD')
+
+currentdir = os.getcwd()
+string = "cp -r %s/Alaska_OpenSubtitles_OSD /Applications/Plex.app/Contents/Resources/Plex/scripts/" % (currentdir)
+string = string.replace("Application Support", "Application\ Support")
+
+if (scriptexists == 0):
+	os.system (string)
